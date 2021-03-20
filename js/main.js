612596d280067758005f7c92a80d4aacd289da9b
MireGo2013 <sobol.v92@gmail.com>
2021-03-20 21:52:54 +0300

    chore: corrected comments

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 const song = document.getElementById('song');
+const video = document.querySelector('.video_conteiner video');
 const play = document.querySelector('.play');
 const outline = document.querySelector('.moving_outline circle');
+const timeDisplay = document.getElementById('time_display');
+const timeSelectBtn = document.querySelector('.time_select');
+const sounds = document.querySelector('.sound_picker');
 const outlineLength = outline.getTotalLength();
-const video = document.querySelector('.video_conteiner video');
-const sounds = document.querySelectorAll('.sound_picker button');
-const timeDisplay = document.querySelector('.time_display');
-const timeSelectBtn = document.querySelectorAll('.time_select button');
 
 let fakeDuration = 600;
 
 outline.style.strokeDasharray = outlineLength;
 outline.style.strokeDashoffset = outlineLength;
-
 
 const app = () => {
 
@@ -19,22 +18,22 @@ const app = () => {
 		checkPlaying(song);
 	});
 
-	// Make btn for style song 
-	sounds.forEach(btn => {
-		btn.addEventListener('click', function () {
-			song.src = this.getAttribute('data-sound');
-			video.src = this.getAttribute('data-video');
+	// Make btn for style song and video
+	sounds.addEventListener('click', function (e) {
+		if (e.target.closest('img')) {
+			song.src = e.target.getAttribute('data-sound');
+			video.src = e.target.getAttribute('data-video');
 			checkPlaying(song);
-		});
-	});
+		}
+	})
 
 	// Make btn for long time song 
-	timeSelectBtn.forEach(btn => {
-		btn.addEventListener('click', function () {
-			fakeDuration = this.getAttribute('data-time');
-			timeDisplay.textContent = `${addZero(Math.floor(fakeDuration / 60))}:${addZero(Math.floor(fakeDuration % 60))}`
-		});
-	});
+	timeSelectBtn.addEventListener('click', function (e) {
+		if (e.target.closest('button')) {
+			fakeDuration = e.target.getAttribute('data-time');
+			timeDisplay.textContent = `${addZero(Math.floor(fakeDuration / 60))}:${addZero(Math.floor(fakeDuration % 60))}`;
+		}
+	})
 
 	// Make play btm
 	const checkPlaying = (song) => {
@@ -65,8 +64,8 @@ const app = () => {
 			video.pause();
 		}
 	};
-	
 };
+
 //Add zero to time < 10
 function addZero(n) {
 	return (parseInt(n, 10) < 10 ? '0' : '') + n;
